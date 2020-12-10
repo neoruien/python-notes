f44f351bc39972d62ad6848ff89784c1a60634f3
successs404 <neoruien@u.nus.edu>
2020-12-10 12:05:26 +0800

    Modify escapechar file

--- HelloWorld/.idea/escapechar.py
+++ HelloWorld/.idea/escapechar.py
@@ -1,23 +1,52 @@
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
-# or
-anotherSplitString = """This string has been
-split over
-several
-lines"""
-print(anotherSplitString)
-# nop
+# =========================================================================
+print("PRINTING STRING")
+
+# Printing string with many quotation marks
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+# Delimiting strings with \ and newline
 unsplitString = """This string has been \
 split over \
 several \
 lines"""
 print(unsplitString)
+# or
+anotherUnsplitString = """The pet shop owner said "No, no, \
+'e's uh,...he's resting"."""
+print(anotherUnsplitString)
 
+# =========================================================================
+
+print("SPLITTING STRING")
+
+# Splitting string with \n
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+# Splitting string with many quotation marks and newlines
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+print(anotherSplitString)
+
+# =========================================================================
+
+print("TABBING STRING")
+
+# Adding tabs to string with \t
 tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
+# =========================================================================
+
+print("PRINTING SPECIAL CHARACTERS")
+
+# Printing special characters with \
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 # or
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 # or
-print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+print("C:\\Users\\timbuchalka\\notes.txt")
+# or (raw string)
+print(r"C:\Users\timbuchalka\notes.txt")

03981768a398d382845a5ee75ae79860c171d101
successs404 <neoruien@u.nus.edu>
2020-12-10 12:18:29 +0800

    Add dynamic and strong typing code

--- HelloWorld/.idea/strings.py
+++ HelloWorld/.idea/strings.py
@@ -5,3 +5,14 @@ print("Printing another type of 'quotes' in strings")
 greeting = "Hello"
 name = input("Please enter your name ")
 print(greeting + ' ' + name)
+
+# Dynamic typing
+age = 24
+print(type(age)) # int --> bind the name/label/variable age to the int value
+age = "2 years"
+print(type(age)) # string --> rebind the label age to the string value
+
+# Strong typing
+name = "Sarah Tan"
+age = 20
+print(name + " is " + age + "years old") # error

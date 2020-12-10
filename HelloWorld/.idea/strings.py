70d0d785d15ae4a6da70ce5948bf04873466c108
successs404 <neoruien@u.nus.edu>
2020-12-10 12:08:38 +0800

    Add input to strings file

--- HelloWorld/.idea/strings.py
+++ HelloWorld/.idea/strings.py
@@ -1,3 +1,7 @@
-print('Python is intersting')
+print('Python is interesting')
 print('Printing "quotes" in strings')
 print("Printing another type of 'quotes' in strings")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + ' ' + name)

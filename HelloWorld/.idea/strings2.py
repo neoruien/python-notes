db35db383bbd6b2d374620c99058c362b0d9bc36
successs404 <neoruien@u.nus.edu>
2020-12-10 15:19:51 +0800

    Do mini challenge regarding strings

--- HelloWorld/.idea/strings2.py
+++ HelloWorld/.idea/strings2.py
@@ -3,3 +3,11 @@ parrot = "Norwegian Blue"
 print(parrot)
 
 print(parrot[3]) # string is an array
+
+print("\nMini challenge:")
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])

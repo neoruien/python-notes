4e774ca169a499d2c18a1d40bebdc8bf7338d959
successs404 <neoruien@u.nus.edu>
2020-12-10 15:30:06 +0800

    Note down negative indexing

--- HelloWorld/.idea/strings2.py
+++ HelloWorld/.idea/strings2.py
@@ -11,3 +11,20 @@ print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+
+print("\n Positive and negative indexing:")
+index = ""
+word = ""
+for i in range(-14, 14): # 14 is the string length
+  index += str(i)
+  word += parrot[i]
+print(index)
+print(word)
+
+print("\nMini challenge (negative indexing):") # negative index = positive index - string length
+print(parrot[-11]) # 11 = 3 - 14
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])

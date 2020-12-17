568ba5cd7655f405bd532714f17b2844bf0d279f
successs404 <neoruien@u.nus.edu>
2020-12-17 17:28:33 +0800

    Edit guessinggame code

--- ProgramFlow/.idea/guessinggame.py
+++ ProgramFlow/.idea/guessinggame.py
@@ -3,6 +3,34 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
+if guess != answer:
+  if guess < answer:
+    print("Please guess higher")
+  else: # guess must be greater than answer
+    print("Please guess lower")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it")
+  else:
+    print("Sorry, you have not guessed correctly")
+else:
+  print("You got it first time")
+
+# Reordered
+if guess == answer:
+  print("You got it first time")
+else:
+  if guess < answer:
+    print("Please guess higher")
+  else: # guess must be greater than answer
+    print("Please guess lower")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it")
+  else:
+    print("Sorry, you have not guessed correctly")
+
+# Code with duplication
 if guess < answer:
   print("Please guess higher")
   guess = int(input())
@@ -20,18 +48,10 @@ elif guess > answer:
 else:
   print("You got it first time")
 
+# Comparing strings with ==
 tree1 = input("Please provide the name of your first tree: ")
 tree2 = input("Please provide the name of your second tree: ")
 if tree1 == tree2:
   print("The trees are the same")
 else:
   print("The trees are different")
-
-x = 5
-y = 7
-if x > y:
-  print("x is greater than y")
-elif x < y:
-  print("x is smaller than y")
-else:
-  print("x equals y")

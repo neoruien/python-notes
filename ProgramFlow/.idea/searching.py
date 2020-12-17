7e88c0f16c355dfefcc0b03bf261d635e7bc921b
successs404 <neoruien@u.nus.edu>
2020-12-17 22:09:28 +0800

    Do shopping more efficiently

--- ProgramFlow/.idea/searching.py
+++ ProgramFlow/.idea/searching.py
@@ -3,8 +3,17 @@ shopping_list = ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice']
 item_to_find = 'spam'
 found_at = None
 
+# method 1 of finding the item's position
 for index in range(len(shopping_list)):
   if shopping_list[index] == item_to_find:
     found_at = index
+    break
 
-print("Item found at position {}".format(found_at))
+# method 2 of finding the item's position
+if item_to_find in shopping_list:
+  found_at = shopping_list.index(item_to_find)
+
+if found_at is not None: # found_at != None
+  print("Item found at position {}".format(found_at))
+else:
+  print("{} not found".format(item_to_find))

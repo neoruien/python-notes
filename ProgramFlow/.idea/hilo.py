dd37d282d31dd32a8de9dd15db6b57d88d6f0574
successs404 <neoruien@u.nus.edu>
2020-12-18 00:39:55 +0800

    Edit hilo file

--- ProgramFlow/.idea/hilo.py
+++ ProgramFlow/.idea/hilo.py
@@ -5,8 +5,8 @@ print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start")
 
 guesses = 1
-while True:
-  print("\tGuessing in the range of {} to {}".format(low, high))
+while low != high: # debugging: right click the breakpoint to include a condition
+  # print("\tGuessing in the range of {} to {}".format(low, high))
   guess = low + (high - low) // 2
   high_low = input("My guess is {}. Should I guess higher or lower? "
                    "Enter h or l, or c if my guess was correct: "
@@ -32,5 +32,7 @@ while True:
   # augmented assignment (more efficient)
   # performs the operation in-place where possible,
   # modifying the original variable
-
+else:
+  print("You thought of the number {}".format(low))
+  print("I got it in {} guesses".format(guesses))
 pass # placeholder for real code

77f899afc54e78d4981c3fa46dff6d99f1f5b847
successs404 <neoruien@u.nus.edu>
2020-12-17 18:37:03 +0800

    Add timestables file

---
+++ ProgramFlow/.idea/timestables.py
@@ -0,0 +1,8 @@
+for i in range(1, 13):
+  for j in range(1, 13):
+    print("{0} times {1} is {2}".format(j, i, i * j))
+  print("-" * 20)
+
+# What happens if we indent the print("-" * 20) line another 4 spaces?
+
+# What happens if we remove the indent completely for the print("-" * 20) line?

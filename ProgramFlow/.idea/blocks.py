60d796ba997a6c89b9da25102e790338fd030865
successs404 <neoruien@u.nus.edu>
2020-12-17 17:10:20 +0800

    Add if-elif-else blocks

--- ProgramFlow/.idea/blocks.py
+++ ProgramFlow/.idea/blocks.py
@@ -1,2 +1,15 @@
 for i in range(1, 13):
   print(i)
+print("*" * 20) # indent vs un-indent
+
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+
+if age < 18:
+  print("Please come back in {0} years".format(18 - age))
+elif age == 900:
+  print("Sorry, Yoda, you die in Return of the Jedi")
+else:
+  print("You are old enough to vote")
+  print("Please put an X in the box")

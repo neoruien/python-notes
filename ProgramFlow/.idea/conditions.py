160867b3d88fedc0d8a8fa76c6ba3957b13504f8
successs404 <neoruien@u.nus.edu>
2020-12-17 18:25:01 +0800

    Add for loop alternative

--- ProgramFlow/.idea/conditions.py
+++ ProgramFlow/.idea/conditions.py
@@ -5,9 +5,14 @@ if 16 <= age <= 65:
   print("Have a good day at work")
 else:
   print("Enjoy your free time")
+# using range
+if age in range(16, 66):
+  print("Have a good day at work")
+else:
+  print("Enjoy your free time")
 # and: Python stops checking as soon as it finds a False condition
 
-print("-" * 20)
+print("-" * 25)
 
 if age < 16 or age > 65:
   print("Enjoy your free time")

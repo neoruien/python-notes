ec075e8cff4dc445049a63c5dd12ed3c132d5f0f
successs404 <neoruien@u.nus.edu>
2020-12-17 22:29:38 +0800

    Do the break-continue practice

---
+++ ProgramFlow/.idea/continuepractice.py
@@ -0,0 +1,16 @@
+## Write a program to print out all the numbers from 0 to 20
+## That aren't divisble by 3 or 5
+## Note that 0 is considered divisible by everything
+
+# Using continue
+for x in range(0, 21):
+  if x % 3 == 0 or x % 5 == 0:
+    continue
+  print(x)
+
+print("*" * 15)
+
+# Without using continue
+for x in range(0, 21):
+  if x % 3 != 0 and x % 5 != 0:
+    print(x)

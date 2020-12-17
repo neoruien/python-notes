07060ac2b0a68efbc8c3d170561afd36c34bb5b5
successs404 <neoruien@u.nus.edu>
2020-12-17 17:18:54 +0800

    Do if-elif-else quiz

--- ProgramFlow/.idea/guessinggame.py
+++ ProgramFlow/.idea/guessinggame.py
@@ -9,3 +9,19 @@ elif guess > answer:
   print("Please guess lower")
 else:
   print("You got it first time")
+
+tree1 = input("Please provide the name of your first tree: ")
+tree2 = input("Please provide the name of your second tree: ")
+if tree1 == tree2:
+  print("The trees are the same")
+else:
+  print("The trees are different")
+
+x = 5
+y = 7
+if x > y:
+  print("x is greater than y")
+elif x < y:
+  print("x is smaller than y")
+else:
+  print("x equals y")
